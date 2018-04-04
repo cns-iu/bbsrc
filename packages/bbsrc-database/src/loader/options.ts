ad4bf64986d84d7b967663b764f0e7df83cc0475
Bruce Herr II <bherr@indiana.edu>
2018-04-04 12:11:51 -0400

    Changes to support running the database in clientdb mode

--- packages/bbsrc-database/src/loader/options.ts
+++ packages/bbsrc-database/src/loader/options.ts
@@ -3,3 +3,4 @@ export const PUBS = '../../raw-data/publications.xlsx';
 export const DB_JSON = '../../raw-data/database.json';
 export const DB_DUMP = '../../raw-data/db-dump.json';
 export const DB_SQLITE = '../../raw-data/db/bbsrc-sqlite.db';
+export const DB_DUMP_URI = '/assets/db-dump.json';
--- packages/bbsrc-database/src/worker.ts
+++ packages/bbsrc-database/src/worker.ts
@@ -1,24 +1,8 @@
 import { createWorker, handleSubscriptions } from 'apollo-link-webworker';
 
 import { schema } from './graphql/schema';
-import { GraphQLContext } from './graphql/context';
 import { pubsub } from './graphql/subscriptions';
-
-import { BBSRCDatabase } from './rxdb/bbsrc-database';
-
-const database = new BBSRCDatabase(false, 'memory');
-const context = new GraphQLContext(database);
-
-fetch('/assets/db-dump.json', { method: 'get' }).then(async function(response) {
-  const dump = await response.json();
-
-  console.log('Importing Dump');
-  const db = await database.get();
-  await db.importDump(dump);
-  console.log('Dump Loaded');
-}).catch(function(err) {
-    console.log(err);
-});
+import { context } from './graphql/client-context';
 
 createWorker({
   schema,
