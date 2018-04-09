42fb58607670f4769d007cca5b5f0bdeda679781
axdanbol <daniel_bolin@yahoo.com>
2018-04-09 16:15:04 -0400

    Better styling for results panel

--- packages/client/src/app/bbsrc-ui/results-panel/results-panel.component.ts
+++ packages/client/src/app/bbsrc-ui/results-panel/results-panel.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import {
+  Component, Input,
+  OnInit,
+  SimpleChanges, ViewEncapsulation
+} from '@angular/core';
+import {
+  trigger, state, style, animate, transition
+} from '@angular/animations';
 import { Observable } from 'rxjs/Observable';
 
 import { FieldV2 as Field, BoundField, Operator } from '@ngx-dino/core';
 import '@ngx-dino/core/src/operators/add/static/access';
+import '@ngx-dino/core/src/operators/add/static/map';
 
 import { BBSRCDatabaseService, Filter, Publication } from 'bbsrc-database';
 
-function simpleField(fieldName: string, label: string): BoundField<string> {
-  return new BoundField<string>(
-    new Field({id: fieldName, label}),
-    Operator.access<any, string>(fieldName)
-  );
+
+function simpleField(fieldName: string, label: string): BoundField<any> {
+  const op = Operator.access<any, string>(fieldName);
+  const field = new Field({
+    id: fieldName, label, initialOp: op, mapping: [
+      ['default', Operator.map((content) => ({type: 'text', content}))]
+    ]
+  });
+
+  return field.getBoundField('default');
 }
+
 
 @Component({
   selector: 'bbsrc-results-panel',
@@ -41,14 +54,15 @@ function simpleField(fieldName: string, label: string): BoundField<string> {
       transition('inactive => active', animate('60ms ease-in')),
       transition('active => inactive', animate('60ms ease-out'))
     ])
-  ]
+  ],
+  encapsulation: ViewEncapsulation.None
 })
-export class ResultsPanelComponent implements OnInit{
+export class ResultsPanelComponent implements OnInit {
   panelState = 'inactive';
   buttonState = 'inactive';
 
   @Input() fields: BoundField<string>[] = [
-    ['author', 'Author'], ['year', 'year'], ['title', 'Title'],
+    ['author', 'Author'], ['year', 'Year'], ['title', 'Title'],
     ['pmid', 'PubMed ID'], ['doi', 'DOI'], ['id', 'GTR ID'],
     ['journalName', 'Journal']
   ].map((s) => simpleField(s[0], s[1]));
@@ -60,11 +74,7 @@ export class ResultsPanelComponent implements OnInit{
 
   constructor(private dataService: BBSRCDatabaseService) { }
 
-  ngOnInit() {}
-
-  ngOnChanges(changes: SimpleChanges) {
-
-  }
+  ngOnInit() { }
 
   public showSubdiscipline(subd_id: any) {
     this.panelState = 'active';
