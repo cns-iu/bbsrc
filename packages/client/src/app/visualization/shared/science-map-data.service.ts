c73c660ecebe102a610499674d1a62d9a08ed5d4
Bruce Herr II <bherr@indiana.edu>
2018-05-31 15:37:05 -0400

    Removed temporary fix for when there are zero results. This is now properly handled in science map

--- packages/client/src/app/visualization/shared/science-map-data.service.ts
+++ packages/client/src/app/visualization/shared/science-map-data.service.ts
@@ -30,10 +30,6 @@ export class ScienceMapDataService {
 
     const subdiscs = this.databaseService.getSubdisciplines(filter);
     this.dataSubscription = subdiscs.subscribe((subdisciplines) => {
-        // FIXME: sciencemap doesn't like having zero results...
-        if (subdisciplines.length === 0) {
-          subdisciplines = [{subd_id: -1, weight: 0}];
-        }
         this.filteredSubdisciplines.next(subdisciplines);
       }
     );
